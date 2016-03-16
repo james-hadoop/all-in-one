220cb577fca0490da47c65d53e7a563b424d0584
James <james_hadoop@163.com>
2016-03-16 23:20:57 +0800

    convert to maven project

--- src/main/java/com/james/common/conf/CommonConf.java
+++ src/main/java/com/james/common/conf/CommonConf.java
@@ -4,9 +4,9 @@ public class CommonConf {
     /*
      * email
      */
-    public static final String MAIL_RECEIVE_BOX = "akajames@126.com";
-    public static final String MAIL_FROM = "akajames@126.com";
+    public static final String MAIL_RECEIVE_BOX = "james@126.com";
+    public static final String MAIL_FROM = "james@126.com";
     public static final String MAIL_HOST = "smtp.126.com";
-    public static final String MAIL_USERNAME = "akajames";
-    public static final String MAIL_PASSWORD = "honeywell2014";
+    public static final String MAIL_USERNAME = "james";
+    public static final String MAIL_PASSWORD = "james123";
 }
