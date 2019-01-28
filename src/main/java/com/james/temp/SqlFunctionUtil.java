459ff8d5d034ac7a3db590c9261a52f6d7de3881
james <james@JamesUbuntu>
2019-01-28 22:07:14 +0800

    add removeSqlFunctionName

--- src/main/java/com/james/temp/SqlFunctionUtil.java
+++ src/main/java/com/james/temp/SqlFunctionUtil.java
@@ -2,51 +2,92 @@ package com.james.temp;
 
 public class SqlFunctionUtil {
 
-	public static void main(String[] args) {
-		String s1 = "r_t_a.SUM(MAX(r_a_a))";
-		String s2 = "r_t_a.MAX(r_a_a)";
-		String s3 = "(MAX(r_a_a))";
-		String s4 = "nvl(a.h,b.i)";
-		String s5 = "hello";
-		String s6 = "nvl(a,b)";
+    public static void main(String[] args) {
+        String s1 = "r_t_a.SUM(MAX(r_a_a))";
+        String s2 = "r_t_a.MAX(r_a_a)";
+        String s3 = "(MAX(r_a_a))";
+        String s4 = "nvl(a.h,b.i)";
+        String s5 = "hello";
+        String s6 = "nvl(a,b)";
+        String s7 = "james.hi";
 
-		String sqlFunction = s6;
+        String sqlFunction = s7;
 
-		System.out.println("before: " + sqlFunction);
-		System.out.println("after:  " + removeSqlFunctionName(sqlFunction));
-	}
+        System.out.println("before: " + sqlFunction);
+        System.out.println("after:  " + removeSqlFunctionName(sqlFunction));
+    }
 
-	/*
-	 * hello -> hello
-	 * 
-	 * r_t_a.SUM(MAX(r_a_a)) -> r_t_a.r_a_a
-	 * 
-	 * nvl(a,b) -> a,b
-	 * 
-	 */
+    /*
+     * hello -> hello
+     * 
+     * r_t_a.SUM(MAX(r_a_a)) -> r_t_a.r_a_a
+     * 
+     * nvl(a,b) -> a,b
+     * 
+     */
 
-	public static String removeSqlFunctionName(String sqlFunction) {
-		if (null == sqlFunction || 0 == sqlFunction.length()) {
-			return null;
-		}
+    public static String removeSqlFunctionName(String sqlFunction) {
+        if (null == sqlFunction || 0 == sqlFunction.length()) {
+            return null;
+        }
 
-		if (!sqlFunction.contains("(")) {
-			return sqlFunction;
-		}
+        String tableName = getTableName(sqlFunction);
+        String tableData = getTableData(sqlFunction);
 
-		if (sqlFunction.contains(".")) {
+        String tableCol = removeBrackets(tableData);
 
-		}
+        System.out.println(tableName);
+        System.out.println(tableData);
+        System.out.println(tableCol);
 
-		int dotIndex = sqlFunction.indexOf(".");
+        if (tableName.equals(tableData)) {
+            return tableCol;
+        }
 
-		int begin = sqlFunction.lastIndexOf("(");
+        return tableName + "." + tableCol;
+    }
 
-		int end = sqlFunction.indexOf(")");
+    private static String removeBrackets(String content) {
+        if (null == content || 0 == content.length()) {
+            return null;
+        }
 
-		String ret = sqlFunction.substring(begin + 1, end);
+        if (!content.contains("(")) {
+            return content;
+        }
 
-		return ret;
-	}
+        return content.substring(content.lastIndexOf("(") + 1, content.indexOf(")"));
+    }
 
+    private static String getTableName(String content) {
+        if (null == content || 0 == content.length()) {
+            return null;
+        }
+
+        if (!content.contains(".")) {
+            return content;
+        }
+
+        if (-1 != content.indexOf("(") && content.indexOf("(") < content.indexOf(".")) {
+            return content;
+        }
+
+        return content.substring(0, content.indexOf("."));
+    }
+
+    private static String getTableData(String content) {
+        if (null == content || 0 == content.length()) {
+            return null;
+        }
+
+        if (!content.contains(".")) {
+            return content;
+        }
+
+        if (-1 != content.indexOf("(") && content.indexOf("(") < content.indexOf(".")) {
+            return content;
+        }
+
+        return content.substring(content.indexOf(".") + 1, content.length());
+    }
 }
